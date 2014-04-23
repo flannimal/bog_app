45e4fc0f292edb7516ca62f69d5bfe1c184ff2a0
flannimal <flanaganm09@gmail.com>
2014-04-23 08:58:09 -0700

    bog app project

--- app/controllers/bogs_controller.rb
+++ app/controllers/bogs_controller.rb
@@ -1,45 +1,49 @@
 class BogsController < ApplicationController
-		def index
-			@creatures = Creature.all
-			render :index
-		end
-		def new
-			render :new
-		end
-		def create
-			  new_creature = params.require(:creature).permit(:name, :description)
-    		@new_creature = Creature.create(new_creature)
-    		redirect_to "/bogs/#{@new_creature.id}"
-    end
+	
+	def index
+		@creatures = Creature.all
+		render :index
+	end
 
-    def show
-    	creature_id = params[:id]
-    	@creature = Creature.find(creature_id)
-    	render :show
-    end
+	def new
+		render :new
+	end
 
-		def edit
-        creature_id = params[:id]
-        @creature = Creature.find(creature_id)
-        render :edit
-		end
+	def list
+		@creatures = Creature.all
+		render :list
+	end
+	
+	def create
+		  new_creature = params.require(:creature).permit(:name, :description)
+  		@new_creature = Creature.create(new_creature)
+  		redirect_to "/bogs/#{@new_creature.id}"
+  end
 
-		def update
-				creature_id = params[:id]
-				creature = Creature.find(creature_id)
-				updated_attributes = params.require(:creature).permit(:name, :description)
-				Creature.update(creature, updated_attributes)
-				redirect_to "/bogs/#{creature_id}"
-		end
+  def show
+  	creature_id = params[:id]
+  	@creature = Creature.find(creature_id)
+  	render :show
+  end
 
-		def delete
-				puts "I am in delete"
-				@creature = Creature.find(params[:id])
-				Creature.destroy(@creature)
-				redirect_to "/bogs"
-				#updated_attributes = params.require(:creature).permit(:name, :description)
-				# Creature.delete(creature)
-				# redirect_to '/bogs'
-		end
+	def edit
+      creature_id = params[:id]
+      @creature = Creature.find(creature_id)
+      render :edit
+	end
+
+	def update
+			creature_id = params[:id]
+			creature = Creature.find(creature_id)			# @creature = fail
+			updated_attributes = params.require(:creature).permit(:name, :description)
+			Creature.update(creature, updated_attributes)     #what did the 2nd argument (nil) do in create method?
+			redirect_to "/bogs/#{creature_id}"
+	end
+
+	def delete
+			@creature = Creature.find(params[:id])
+			Creature.destroy(@creature)					# destory is an active record method, not rails or sql...?
+			redirect_to "/bogs"
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,23 @@ BogApp::Application.routes.draw do
 
   root to: 'bogs#index'
 
-  get '/bogs', to: 'bogs#index'
+  get '/bogs', to: 'bogs#index'     #/bogs is the menu page
 
-  get '/bogs/new', to: 'bogs#new'
+  get '/bogs/new', to: 'bogs#new'     #new goes to /bogs/:id then to /show
 
-  post '/bogs', to: 'bogs#create'
+  get '/bogs/list', to: 'bogs#list'		# list is an index  (has to go before show)
 
-  get '/bogs/:id', to: 'bogs#show'
+  get '/bogs/:id', to: 'bogs#show'		#1 item display
 
-  get '/bogs/:id/edit', to: 'bogs#edit'
+  get '/list/:id/edit', to: 'bogs#edit'   
 
   put '/bogs/:id', to: 'bogs#update'
 
   get '/bogs/:id/delete', to: 'bogs#delete'
+
+  post '/bogs', to: 'bogs#create'
+
 end
+
+
+"bogs/new"
